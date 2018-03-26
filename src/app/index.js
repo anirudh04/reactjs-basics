c92d21da13243141aabe272467a1793b3cb295a9
Anirudh Khandelwal <anirudhkhandelwal04@gmail.com>
2018-03-26 10:56:34 +0530

    Component Lifecycle

--- src/app/index.js
+++ src/app/index.js
@@ -8,7 +8,9 @@ class App extends React.Component {
     constructor(){
 		super();
 		this.state={
-			homeLink: "Home"
+			homeLink: "Home",
+			homeMounted: true
+
 			};
 	}
 
@@ -24,7 +26,26 @@ class App extends React.Component {
 
 	}
 
+	onChangeHomeUnmounted(){
+		this.setState({
+			homeMounted:!this.state.homeMounted
+		});
+	}
+
 	render() {
+		let homeCmp = "";
+		if(this.state.homeMounted)
+		{
+			homeCmp = (
+			<Home
+				name={"Max"}
+				Initialage={28} 
+				greet={this.onGreet}
+				changeLink={this.onChangeLinkName.bind(this)}
+				initialLinkName = {this.state.homeLink}
+			/>
+		);		
+		}
 		return(
 			<div className="container">
 						<div className="row">
@@ -34,12 +55,12 @@ class App extends React.Component {
 						</div>
 					<div className="row">
 						<div className=".col-xs-10 col-xs-offset-1">
-							<Home name={"Max"}
-								  Initialage={28} 
-							      greet={this.onGreet}
-								  changeLink={this.onChangeLinkName.bind(this)}
-								  initialLinkName = {this.state.homeLink}
-							/>		
+						  {homeCmp}
+						</div>
+					</div>
+					<div className="row">
+						<div className=".col-xs-10 col-xs-offset-1">
+						  <button onClick={this.onChangeHomeUnmounted.bind(this)} className="btn btn-primary"> (Un)Mount Home Component</button>
 						</div>
 					</div>
 			</div>
